17e2690a3463f35f6bf22fe15f65e70c7b8bcfd3
kar <hi@kar.gs>
2023-01-06 14:06:05 +0100

    add sync scripts

--- sync-blocklist.sh
+++ sync-blocklist.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-echo "Syncing default blocklist"
+echo "Syncing default blocklist..."
 
-grep -v "covid" ../landing-github-pages/mirror/v5/oisd/light/hosts.txt > cleaned-hosts.txt
-cp cleaned-hosts.txt app/src/main/assets/default_blocklist
+cd landing-github-pages
+git checkout master
+git pull
+
+hash=$(git describe --abbrev=4 --always --tags --dirty)
+commit="sync: update default blocklist to: $hash"
+
+echo $commit
+
+grep -v "covid" mirror/v5/oisd/light/hosts.txt > cleaned-hosts.txt
+cp cleaned-hosts.txt ../app/src/libre/assets/default_blocklist
 rm cleaned-hosts.txt
-cd app/src/main/assets/
+cd ../app/src/libre/assets/
 rm default_blocklist.zip
 zip -e default_blocklist.zip default_blocklist
 rm default_blocklist
 cd ../../../
+
+git commit -am "$commit"
+
 echo "Done"
-git add .
-git ci -am "sync: update default blocklist"
